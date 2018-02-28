199942ea14f73e83f2a049ab3919bb550821262c
Darren Wu <darrenchwu@gmail.com>
2018-02-28 23:14:01 +0800

    update pt-update.sh

--- pt-update.sh
+++ pt-update.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 # Author:  Darren (darrenchwu)
-# Origin author: Xlemon
 
 ##
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-currentDir=$PWD
-
 # Download newest PT
 curl https://api.github.com/repos/taniman/profit-trailer/releases/latest|grep -Po '"browser_download_url": *\K"[^"]*"'|sed 's/"\|,//g'|xargs wget -O pt.zip
 
+# Unzip jar file
 unzip -j -o pt.zip ProfitTrailer/ProfitTrailer.jar
 
+#remove downloaded file
 rm  pt.zip
 
-
-printf "Done!\n"
