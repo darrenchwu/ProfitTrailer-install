03784e7f94cc8985c03ff7f1f633535baef5808a
Darren Wu <darrenchwu@gmail.com>
2018-05-08 08:18:22 +0800

    Mod pt-update.sh for PT 2.0.3

--- pt-update.sh
+++ pt-update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Author:  Darren (darrenchwu)
+# Author:  Darren (TG @darrenchwu)
+# Mod 2018/5/8 for PT 2.0.3
 
 ##
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
@@ -7,9 +8,12 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 # Download newest PT
 curl -s https://api.github.com/repos/taniman/profit-trailer/releases/latest|grep -Po '"browser_download_url": *\K"[^"]*"'|sed 's/"\|,//g'|xargs wget -O pt.zip
 
-# Unzip jar file
-unzip -j -o pt.zip ProfitTrailer/ProfitTrailer.jar
+# Unzip file
+unzip -j -o -d pt-unzip pt.zip
+
+# Copy unzip jar to replace existing jar
+cp pt-unzip/ProfitTrailer.jar ../
 
 #remove downloaded file
-rm  pt.zip
+rm  -rf pt.zip pt-unzip
 
